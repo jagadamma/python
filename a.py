4924b869579f99059078ebe97f71e9a148525c6d
jagadamma <96439002+jagadamma@users.noreply.github.com>
2023-11-08 03:00:53 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,7 +1,10 @@
 from flask import Flask
-abi=Flask(__name__)
-@abi.route('/')
-def a():
-    return("hello shivam")
-if __name__ == "__main__":
-    abi.run(debug=True,host='0.0.0.0')
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')

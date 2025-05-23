9c62302b8262c05c61bbb676368ef57e01902e55
jagadamma <96439002+jagadamma@users.noreply.github.com>
2025-05-23 20:42:38 +0530

    Update a.py

--- a.py
+++ a.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "superstar!"
+    return "Hi this is Abilash Deploying python app in GCP cloud run"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
